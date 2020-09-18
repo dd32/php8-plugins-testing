7f5e5b0e81e1bf7c61821de57c5baa9af1a95eb5
Dion Hulse <contact@dd32.id.au>
2020-09-18 15:47:46 +1000

    Simpler output for PHP parse errors.

--- run_checks.php
+++ run_checks.php
@@ -2,8 +2,14 @@
 <?php
 $exit_status = 0;
 
-// Run PHP CS PHPCompatibility checks.
+// Compat function for testing on older PHP.
+if ( ! function_exists( 'str_contains' ) ) {
+	function str_contains( $haystack, $needle ) {
+		return '' === $needle || false !== strpos($haystack, $needle );
+	}
+}
 
+// Run PHP CS PHPCompatibility checks.
 exec( ( file_exists( 'vendor/bin/phpcs' ) ? 'vendor/bin/phpcs' : 'phpcs' ) . ' -s plugin', $output, $returnval );
 echo implode( "\n", $output );
 if ( $returnval > 0 ) {
@@ -29,7 +35,7 @@ if ( $returnval > 0 ) {
 	$output = preg_replace('/\e[[][A-Za-z0-9];?[0-9]*m?/', '', $output );
 
 	// Remove the last two lines, they're the errata after the test.
-	$output = array_slice( $output, 0, count( $output ) - 3 );
+	$output = array_slice( $output, 0, -3 );
 
 	echo '::error::' . implode( '%0A', $output ) . "\n";
 	$exit_status = 1;
@@ -39,11 +45,13 @@ if ( $returnval > 0 ) {
 exec( 'find plugin -name "*.php" -not -name "*polyfill*"', $files, $returnval );
 foreach ( (array) $files as $php_file ) {
 	$output = [];
-	exec( 'php -l ' . escapeshellarg( $php_file ), $output );
+	$php_file = preg_replace( '|^plugin/|', '', $php_file );
+
+	exec( 'cd plugin && php -l ' . escapeshellarg( $php_file ) . ' 2>&1', $output );
 	echo implode( "\n", $output ) . "\n";
 
 	if ( ! str_contains( $output[0], 'No syntax errors detected' ) ) {
-		echo '::error::' . implode( '%0A', $output ) . "\n";
+		echo '::error::' . implode( '%0A', array_slice( $output, 0, -1 ) ) . "\n";
 		$exit_status = 1;
 	}
 }
